dadd6eaf570814ca94158dee1aef83cf7ffebdbe
Max <37291989+maxberndt@users.noreply.github.com>
2018-07-15 19:24:40 +0200

    Errors

--- client.c
+++ client.c
@@ -5,12 +5,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+    fprintf(stderr, msg);
+    exit(1);
 }
 
 int main(int argc, char *argv[])
@@ -20,36 +20,46 @@ int main(int argc, char *argv[])
     struct hostent *server;
 
     char buffer[256];
+
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+       printf("Usage: %s hostname port\n", argv[0]);
+       error("Wrong usage\n");
     }
+
+    int length = strlen(argv[0]);
+    printf("%i",length);
+
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+
+    if (sockfd < 0){
+        error("ERROR opening socket\n");
     }
+
+
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL) {
+        error("ERROR, no such host\n");
+    }
+
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
+    bcopy((char *)server->h_addr,
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     printf("Please enter the message: ");
     bzero(buffer,256);
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
+    if (n < 0)
          error("ERROR writing to socket");
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0) 
+    if (n < 0)
          error("ERROR reading from socket");
     printf("%s\n",buffer);
     close(sockfd);
