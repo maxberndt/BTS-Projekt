0e4b66730ef7841ab77c6e3891d2681d65a00548
Max <37291989+maxberndt@users.noreply.github.com>
2018-07-15 21:07:06 +0200

    ssss

--- getdht11.c
+++ getdht11.c
@@ -12,17 +12,41 @@ void error();
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
+    portno = 2000;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
-
-    if (argc < 3) {
-       printf("Usage: %s hostname port\n", argv[0]);
-       error("Wrong usage\n");
+    if (argc < 2) {
+        error("First parameter must be the servers IP-Address\n");
     }
 
-    portno = atoi(argv[2]);
+    while ((++argv)[0])
+    {
+        if (argv[0][0] == '-' )
+        {
+            switch (argv[0][1])  {
+
+                default:
+                    printf("Unknown parameter -%c\n\n", argv[0][1]);
+                    break;
+                case 'h':
+                    printf("Help: BLABLABLA\n\n");
+                    break;
+                case 'p':
+                    if (argv[0+1]!=NULL){
+                        printf("Port: %s\n\n", argv[0+1]);
+                        portno = atoi(argv[0+1]);
+                    } else {
+                        printf("Port not valid! Using default port...\n");
+                    }
+                    break;
+            }
+        }
+
+    }
+
+    char buffer[256];
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0){
@@ -39,12 +63,12 @@ int main(int argc, char *argv[])
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
     serv_addr.sin_port = htons(portno);
 
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
-      error("ERROR connecting");
+        error("ERROR connecting");
     }
 
     printf("Press Enter to get values...\n");
@@ -52,14 +76,14 @@ int main(int argc, char *argv[])
     fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0){
-      error("ERROR writing to socket\n");
+        error("ERROR writing to socket\n");
     }
 
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
 
     if (n < 0){
-      error("ERROR reading from socket\n");
+        error("ERROR reading from socket\n");
     }
 
     printf("%s\n",buffer);
