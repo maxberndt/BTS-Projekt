8d6f96c119f60af30174d6b6c167eb76c9217758
Max <37291989+maxberndt@users.noreply.github.com>
2018-07-15 21:01:23 +0200

    dd

--- getdht11.c
+++ getdht11.c
@@ -6,91 +6,69 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <argp.h>
 
-
-void error(const char *msg){
-    fprintf(stderr, msg);
-    exit(1);
-}
+void error();
 
 int main(int argc, char *argv[])
 {
-
-    if (argc < 2) {
-        error("First parameter must be servers IP-Address\n");
-    }
-
-    int portnumber = 2000;
-
-    while ((++argv)[0])
-    {
-        if (argv[0][0] == '-' )
-        {
-            switch (argv[0][1])  {
-
-                default:
-                    printf("Unknown parameter -%c\n\n", argv[0][1]);
-                    break;
-                case 'h':
-                    printf("Help: BLABLABLA\n\n");
-                    break;
-                case 'p':
-                    verbose = 1;
-                    break;
-                case 'p':
-                    if (argv[0+1]!=NULL){
-                        printf("Port: %s\n\n", argv[0+1]);
-                        portnumber = atoi(argv[0+1]);
-                    } else {
-                        printf("Port not valid! Using default port...\n");
-                    }
-                    break;
-            }
-        }
-
-    }
-
-    int sock, portno, n;
-    struct sockaddr_in serv_addr; //structur erstellen
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0){
+    if (argc < 3) {
+       printf("Usage: %s hostname port\n", argv[0]);
+       error("Wrong usage\n");
+    }
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0){
         error("ERROR opening socket\n");
     }
 
+
     server = gethostbyname(argv[1]);
+
     if (server == NULL) {
-        error("ERROR, host is not found\n");
+        error("ERROR, no such host\n");
     }
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
 
-    serv_addr.sin_port = htons(portnumber);
-    if (connect(sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
-        error("ERROR connecting\n");
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
+      error("ERROR connecting");
     }
 
-    printf("Press Enter to get values");
+    printf("Press Enter to get values...\n");
+    bzero(buffer,256);
     fgets(buffer,255,stdin);
-    n = write(sock,buffer,strlen(buffer));
+    n = write(sockfd,buffer,strlen(buffer));
     if (n < 0){
-        error("ERROR writing to socket\n");
+      error("ERROR writing to socket\n");
     }
 
     bzero(buffer,256);
-    n = read(sock,buffer,255);
+    n = read(sockfd,buffer,255);
+
     if (n < 0){
-        error("ERROR reading from socket\n");
+      error("ERROR reading from socket\n");
     }
+
     printf("%s\n",buffer);
-    close(sock);
+    close(sockfd);
     return 0;
+}
+
+void error(const char *msg)
+{
+    fprintf(stderr, msg);
+    exit(1);
 }
---
+++ getdht11_2.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <argp.h>
+
+
+void error(const char *msg){
+    fprintf(stderr, msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2) {
+        error("First parameter must be servers IP-Address\n");
+    }
+
+    int portnumber = 2000;
+
+    while ((++argv)[0])
+    {
+        if (argv[0][0] == '-' )
+        {
+            switch (argv[0][1])  {
+
+                default:
+                    printf("Unknown parameter -%c\n\n", argv[0][1]);
+                    break;
+                case 'h':
+                    printf("Help: BLABLABLA\n\n");
+                    break;
+                case 'p':
+                    verbose = 1;
+                    break;
+                case 'p':
+                    if (argv[0+1]!=NULL){
+                        printf("Port: %s\n\n", argv[0+1]);
+                        portnumber = atoi(argv[0+1]);
+                    } else {
+                        printf("Port not valid! Using default port...\n");
+                    }
+                    break;
+            }
+        }
+
+    }
+
+    int sock, portno, n;
+    struct sockaddr_in serv_addr; //structur erstellen
+    struct hostent *server;
+
+    char buffer[256];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0){
+        error("ERROR opening socket\n");
+    }
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        error("ERROR, host is not found\n");
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+
+    serv_addr.sin_port = htons(portnumber);
+    if (connect(sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
+        error("ERROR connecting\n");
+    }
+
+    printf("Press Enter to get values");
+    fgets(buffer,255,stdin);
+    n = write(sock,buffer,strlen(buffer));
+    if (n < 0){
+        error("ERROR writing to socket\n");
+    }
+
+    bzero(buffer,256);
+    n = read(sock,buffer,255);
+    if (n < 0){
+        error("ERROR reading from socket\n");
+    }
+    printf("%s\n",buffer);
+    close(sock);
+    return 0;
+}
